56507fb7ba956b51c5de25d511258e2063369b68
bpaquier <bastien.paquier@gmail.com>
2022-02-04 17:44:27 +0100

    set form template

--- src/Components/Global/Form/index.tsx
+++ src/Components/Global/Form/index.tsx
@@ -2,29 +2,53 @@ import classNames from "classnames/bind";
 import { ReactChildren } from "react";
 
 import Title from "src/Components/Ui-kit/Typo/Title";
-import { JsxElement } from "typescript";
+import Input, { InputProps } from "src/Components/Ui-kit/Form/Input";
+import SquaredCta, {
+  SquaredCtaProps,
+} from "src/Components/Ui-kit/Cta/SquaredCta";
+import CheckBox, { CheckBoxProps } from "src/Components/Ui-kit/Form/Checkbox";
 
 import css from "./styles.module.scss";
 const cx = classNames.bind(css);
 
-export interface FormProps {
-  className?: string;
-	title: string;
-	children: JSX.Element | JSX.Element[];
+interface BlocProps {
+  name?: "input" | "cta" | "checkbox";
+  inputProps?: InputProps;
+  ctaProps?: SquaredCtaProps;
+  checkboxProps?: CheckBoxProps;
 }
 
-export default function Form({ 
-  className, 
-	title,
-	children
- }: FormProps) {
+export interface FormProps {
+  className?: string;
+  title: string;
+  blocs?: BlocProps[];
+}
+
+export default function Form({ className, title, blocs }: FormProps) {
+  const blocSerializer = ({
+    name,
+    inputProps,
+    ctaProps,
+    checkboxProps,
+  }: BlocProps) => {
+    switch (name) {
+      case "input":
+        return <Input className={css.input} {...inputProps} />;
+      case "cta":
+        return <SquaredCta {...ctaProps} />;
+      case "checkbox":
+        return <CheckBox {...checkboxProps} />;
+      default:
+        return null;
+    }
+  };
 
   return (
     <div className={cx(css.Form, className)}>
-			<div className={css.content}>
-				<Title content={title} heading="h1" className={css.title}/>
-				{children}
-			</div>
+      <div className={css.content}>
+        <Title content={title} heading="h1" className={css.title} />
+        {blocs && blocs?.map((bloc) => blocSerializer(bloc))}
+      </div>
     </div>
-  )
+  );
 }
--- src/Pages/SignIn/index.tsx
+++ src/Pages/SignIn/index.tsx
@@ -3,7 +3,6 @@ import classNames from "classnames/bind";
 import Form from "src/Components/Global/Form";
 import Input from "src/Components/Ui-kit/Form/Input";
 
-
 import css from "./styles.module.scss";
 const cx = classNames.bind(css);
 
@@ -14,15 +13,39 @@ interface SignInProps {
 export default function SignIn({ className }: SignInProps) {
   return (
     <div className={cx(css.Signin, className)}>
-      <Form title="S'inscrire">
-        <Input
-          type="text"
-          name="entity"
-          placeholder="DOE Jean"
-          id="signin-name"
-          label="Nom Prénom"
-        />
-      </Form>
+      <Form
+        title="S'inscrire"
+        blocs={[
+          {
+            name: "input",
+            inputProps: {
+              type: "text",
+              placeholder: "Nom Prénom",
+              label: "Nom Prénom",
+              name: "entity",
+              id: "signin-entity",
+            },
+          },
+          {
+            name: "input",
+            inputProps: {
+              type: "mail",
+              placeholder: "Mail",
+              label: "Mail",
+              name: "mail",
+              id: "signin-mail",
+            },
+          },
+          {
+            name: "checkbox",
+            checkboxProps: {
+              label: "souviens",
+              name: " checkbox",
+              id: "signin-checkbox",
+            },
+          },
+        ]}
+      />
     </div>
-  )
+  );
 }
